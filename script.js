68936a5698b3ada4e9cbebb6a94cf77d2e632352
009kings <009kings@gmail.com>
2022-03-23 00:41:51 -0700

    customized babeeey

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let nameHeaders = document.querySelectorAll('.split')
-let name = window.prompt('What is your name?')
 
-nameHeaders[0].textContent = nameHeaders[1].textContent = name;
+document.querySelector('button').addEventListener('click', e => {
+  let name = window.prompt('What is your name?')
+  nameHeaders[0].textContent = nameHeaders[1].textContent = name;
+})
